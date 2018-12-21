a8f6279fe841a3949999534d7ed18758b0360edf
Oleksandr <avkomq@gmail.com>
2018-12-21 16:22:21 -0500

    Add Saga and SagaTests classes.

---
+++ TransactionUtilities.Tests/SagaTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace TransactionUtilities.Tests
+{
+    [TestClass]
+    public class SagaTests
+    {
+        [TestMethod]
+        public void TestMultipleCommits()
+        {
+            // Arrange
+            Mock<Action> compensation0 = new Mock<Action>();
+            compensation0.Setup(m => m());
+
+            Mock<Action> compensation1 = new Mock<Action>();
+            compensation0.Setup(m => m());
+
+            Mock<Action> compensation2 = new Mock<Action>();
+            compensation0.Setup(m => m());
+
+            Exception exception = null;
+
+            // Act
+            try
+            {
+                using (Saga saga = new Saga())
+                {
+                    DoAction0();
+                    saga.RegisterCompensation(compensation0.Object);
+
+                    saga.Commit();
+
+                    DoAction1();
+                    saga.RegisterCompensation(compensation1.Object);
+
+                    DoAction2();
+                    saga.RegisterCompensation(compensation2.Object);
+
+                    saga.Commit();
+                }
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+
+            // Assert
+            Assert.IsInstanceOfType(exception, typeof(ApplicationException));
+            compensation0.Verify(m => m(), Times.Never);
+            compensation1.Verify(m => m(), Times.Once);
+            compensation2.Verify(m => m(), Times.Never);
+        }
+
+        private void DoAction0()
+        {
+        }
+
+        private void DoAction1()
+        {
+        }
+
+        private void DoAction2()
+        {
+            throw new ApplicationException();
+        }
+    }
+}
---
+++ TransactionUtilities/Saga.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TransactionUtilities
+{
+    public class Saga : IDisposable
+    {
+        private List<Action> _compensations;
+
+        /// <summary>
+        /// Register a compensation action that will be run in case of rolling back the saga.
+        /// </summary>
+        /// <param name="compensation">The compensation action.</param>
+        public void RegisterCompensation(Action compensation)
+        {
+            if (compensation == null)
+            {
+                throw new ArgumentNullException(nameof(compensation));
+            }
+
+            if (_compensations == null)
+            {
+                _compensations = new List<Action>();
+            }
+
+            _compensations.Add(compensation);
+        }
+
+        /// <summary>
+        /// Commits the saga. Commit can be called multiple times during the saga.
+        /// All non-committed actions will be rolled back on Dispose() call.
+        /// Dispose() will call all compensation actions registered after the last Commit() call.
+        /// </summary>
+        public void Commit()
+        {
+            _compensations = null;
+        }
+
+        /// <summary>
+        /// Rolls back all non-committed actions. Calls all compensation actions registered after the last Commit() call. 
+        /// </summary>
+        public void Dispose()
+        {
+            if (_compensations != null)
+            {
+                for (int i = _compensations.Count - 1; i >= 0; i--)
+                {
+                    _compensations[i]();
+                }
+            }
+        }
+    }
+}
